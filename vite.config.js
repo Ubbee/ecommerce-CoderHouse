33c35ebbf940f5dc3f5b835529b4fbd875d7cfd4
Santiago Curotto <santiago.curotto.98@gmail.com>
2024-10-26 13:47:36 -0300

    eliminacion de codigo redundante

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/ecomerce-CoderHouse/',
-  esbuild: {
-    loader: 'jsx', // Usa JSX en todos los archivos que usen esta extensión
-    include: /src\/.*\.jsx?$/, // Apunta a todos los archivos .js y .jsx en `src`
+  build: {
+    rollupOptions: {
+      external: ['history', 'sweetalert2', 'sweetalert2/dist/sweetalert2.min.css']
+    }
   }
 })
-
 
