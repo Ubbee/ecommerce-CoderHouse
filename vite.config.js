91aa385abc8ab9cf7c5b385767d0addd67667ea8
Santiago Curotto <santiago.curotto.98@gmail.com>
2024-10-26 13:42:37 -0300

    configuracion despliegue automatico en Github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: '/ecomerce-CoderHouse/',
   build: {
     rollupOptions: {
       external: ['history', 'sweetalert2', 'sweetalert2/dist/sweetalert2.min.css']
